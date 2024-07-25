ca3e2e7e9f995973fec1bd654d8bbac5e4adef17
kamchykulmatov <kulmatovkamchy9@gmail.com>
2024-07-25 17:02:38 +0600

    hw2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        state(45,34);
+        state(24,14);
+        state(75,-64);
+        state(35,-14);
+        state(45,-34);
+
+    }
+
+    public static void state(int age, int temperature) {
+        String result = choice(age, temperature);
+        System.out.println("Возраст: " + age + "temperature: " + temperature ". " + "Result: " + result);
+    }
+
+    public static String choice(int age, int temperature) {
+        if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
+                (age < 20 && age >= 0 && temperature >= 0 && temperature <= 28) ||
+                (age > 45 && temperature >= -10 && temperature <= 25)) {
+            return ("можно идти гулять");
+        } else {
+            return ("Оставайтесь дома");
+
+        }
     }
 }
